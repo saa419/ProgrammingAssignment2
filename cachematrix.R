f0551406d0e7cd5357b626bebffe2a5c9f61fae5
SantiagoA <Santiago.A.Andujar@gmail.com>
2015-01-16 00:32:55 -0500

    More tab fixes in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,35 +6,35 @@
 ## Creates a special matrix object that can cache its inverse
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(m = matrix()) {		## input m will be a matrix
+makeCacheMatrix <- function(m = matrix()) {	## input m will be a matrix
 
 
-    i <- NULL	##  i will be our 'inverse' and it's reset to NULL every 
-					##  time makeCacheMatrix is called
+	i <- NULL	##  i will be our 'inverse' and it's reset to NULL every 
+			##  time makeCacheMatrix is called
 
 	set <- function(matrix) {	## takes an input matrix
-			m <<- matrix		## saves the input matrix
-			i <<- NULL		## resets the inverse to NULL, basically what happens when a new object is generated.
+		m <<- matrix	## saves the input matrix
+		i <<- NULL	## resets the inverse to NULL, basically what happens when a new object is generated.
     }
 
 					##  note these next three functions are defined but not run when makeVector is called.
 					##  instead, they will be used by cacheSolve() to get values for x or for
 					##  i (inverse) and for setting the inverse.  These are usually called object 'methods'
 
-    get <- function() { m }   ## this function returns the value of the original matrix
+	get <- function() { m }	## this function returns the value of the original matrix
     
-    setinverse <- function(inverse)  { i <<- inverse }
+	setinverse <- function(inverse)  { i <<- inverse }
 					## this is called by cachemean() during the first cacheSolve()
 					## access and it will store the value using superassignment
         
-    getinverse <- function() { i }
+	getinverse <- function() { i }
 					## this will return the cached value to cacheSolve() on
 					##  subsequent accesses
 
 
 	list( set = set, get = get,	##  OK, this is accessed each time makeVector() is called,       
-         setinverse = setinverse,	##   that is, each time we make a new object.  This is a list of 
-         getinverse = getinverse)	##  the internal functions ('methods') so a calling function
+	setinverse = setinverse,	##   that is, each time we make a new object.  This is a list of 
+	getinverse = getinverse)	##  the internal functions ('methods') so a calling function
 						##   knows how to access those methods.                        
 }
 
@@ -49,8 +49,8 @@ cacheSolve <- function(x, ...) {	## the input x is an object created by makeVect
     ## Just return the inverse if its already set
     if(!is.null(m)) {	## if inverse was already cached (not NULL) ...
             message("getting cached data")		# ... send this message to the console
-            return(m)		## ... and return the inverse ... "return" ends 
-                        		##   the function cacheSolve(), note
+            return(m)	## ... and return the inverse ... "return" ends 
+                        	##   the function cacheSolve(), note
     }
 
     data <- x$get()	## we reach this code only if x$getinverse() returned NULL
@@ -63,7 +63,3 @@ cacheSolve <- function(x, ...) {	## the input x is an object created by makeVect
     
     m	# return the matrix to the code that called this function
 }
-
-
-
-
